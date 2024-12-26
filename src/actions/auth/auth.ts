7747766b85201ff717fcc84293bfbf4e4cbd0704
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2024-12-25 20:22:09 -0600

    Create auth actions

---
+++ src/actions/auth/auth.ts
@@ -0,0 +1,54 @@
+import inTouchIoApi from "../../config/api/inTouchIoApi";
+import { UserMapper } from "../../infrastructure/mappers/user.mapper";
+
+export const authLogin = async(email: string, password: string) => {
+    console.log('eooo')
+    try {
+        const { data } = await inTouchIoApi.post('/auth/login', { email, password });
+
+        const token = data.token
+        const user = UserMapper.toEntity(data);
+
+        console.log('aaaaa')
+
+        return {
+            user,
+            status: 'authenticated',
+            token,
+            errorMessage: ''
+        }
+
+    } catch (error: any) {
+        console.log(error)
+        if (error instanceof TypeError) {
+            return {
+                status: 'unauthenticated',
+                user: undefined,
+                token: undefined,
+                errorMessage: error.message,
+            };
+        }
+
+        return {
+            status: 'unauthenticated',
+            user: undefined,
+            token: undefined,
+            errorMessage: error.response.data?.error || 'Wrong credentials'
+        };
+    }
+}
+
+export const authCheckStatus = async () => {
+    try {
+        const { data } = await inTouchIoApi.get('/auth');
+
+        return {
+            user: data.user
+        }
+    } catch (error) {
+        console.log(error);
+        return {
+            user: undefined
+        }
+    }
+}
