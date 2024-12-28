0705f1cf479c8a60137db27eedf7dc7e8a4280a4
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2024-12-27 22:02:27 -0600

    Wrap App with AuthProvider and SocketProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import 'react-native-gesture-handler';
 
 import { StackNavigator } from './presentation/navigator/StackNavigator';
 import { ThemeContextProvider } from './presentation/context/ThemeContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { AuthProvider } from './presentation/providers/AuthProvider';
+import { SocketProvider } from './presentation/providers/SocketProvider';
+
+const queryClient = new QueryClient();
 
 export const App = () => {
   return(
+    <QueryClientProvider client={queryClient}>
       <ThemeContextProvider>
-        <StackNavigator />
+        <AuthProvider>
+          <SocketProvider>
+            <StackNavigator />
+          </SocketProvider>
+        </AuthProvider>
       </ThemeContextProvider>
+    </QueryClientProvider>
   )
 }
