7128f6736f774b28d89ddaaf78894fba009e6435
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2024-12-29 20:00:20 -0600

    Set socker url variables

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -3,4 +3,7 @@ declare module '@env' {
     export const API_URL: string;
     export const API_URL_IOS: string;
     export const API_URL_ANDROID: string;
+    export const SOCKET_URL: string;
+    export const SOCKET_URL_IOS: string;
+    export const SOCKET_URL_ANDROID: string;
 }
