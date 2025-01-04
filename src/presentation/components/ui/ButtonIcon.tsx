eb1dd4f496e54403626343b1f034918f64622e33
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-01-04 09:01:11 -0600

    Create ButtonIcon component

---
+++ src/presentation/components/ui/ButtonIcon.tsx
@@ -0,0 +1,55 @@
+import { Pressable, StyleSheet, View } from "react-native"
+import { CustomIcon } from "./CustomIcon";
+import { Text } from "react-native-paper";
+
+interface Props {
+    icon: string;
+    iconColor: string;
+    description: string;
+    onClick: () => void;
+}
+
+export const ButtonIcon = ({ icon, iconColor, description ,onClick }: Props) => {
+  return (
+    <Pressable style={style.container} onPress={onClick}>
+        <View style={[style.iconContainer, {backgroundColor: iconColor}]}>
+            <CustomIcon iconName={icon} size={25} style={style.icon} />
+        </View>
+        <Text style={style.description}>
+            {description}
+        </Text>
+    </Pressable>
+  )
+}
+
+const style = StyleSheet.create({
+    container: {
+        width: 50,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    iconContainer: {
+        borderRadius: 50,
+        width: 50,
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    icon: {
+        color: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 25,
+        height: 25,
+        alignContent: 'center'
+    },
+    description: {
+        color: 'black',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        textAlign: 'center'
+    }
+});
+
