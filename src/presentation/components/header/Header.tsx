07747563122532c6649eb9242c6e6525280f392f
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-01-05 20:06:12 -0600

    Add animated modal to show settings and logout buttons

--- src/presentation/components/header/Header.tsx
+++ src/presentation/components/header/Header.tsx
@@ -1,30 +1,55 @@
-import { Image, StyleSheet, View } from "react-native"
+import { Image, Pressable, StyleSheet, View } from "react-native"
 import { Text } from "react-native-paper"
 import { useAuthStore } from "../../store/auth/useAuthStore"
+import { CustomIcon } from "../ui/CustomIcon";
+import { ModalTopAnimation } from "../ui/ModalTopAnimation";
+import { useState } from "react";
+import { useAnimation } from "../../hooks/useAnimation";
 
 export const Header = () => {
 
     const { user } = useAuthStore();
+    const { animatedTop, startMovingTopPosition } = useAnimation();
+
+    const [isModalVisible, setModalIsVisible] = useState(false);
+
+
+    const resizeBox = (toValue: number, initialPosition: number) => {
+        toValue >= 0 && setModalIsVisible(true);
+        startMovingTopPosition({
+            initialPosition: initialPosition,
+            duration: 300,
+            toValue,
+            callback: () => toValue < 0 && setModalIsVisible(false)
+        })
+        }
   return (
     <View style={style.container}>
         <View style={style.textContainer}>
             <Text style={{fontSize: 14}}>Hello {user?.name},</Text>
             <Text variant="displaySmall" style={{fontWeight: '900'}}>In-Touch IO</Text>
         </View>
-        
-        <Image 
-            source={{
-                uri: user?.pictureUrl
-            }}
-            style = {[
-                {
-                width: 55,
-                height: 55,
-                borderRadius: 50
-                }
-            ]}
-            
-        />
+
+        <Pressable style = {style.imageContainer} onPress={() => resizeBox(1, -300)}>
+            <Image 
+                source={{
+                    uri: user?.pictureUrl
+                }}
+                style = {[
+                    {
+                    width: 55,
+                    height: 55,
+                    borderRadius: 50
+                    }
+                ]}
+                
+            />
+   
+            <CustomIcon size={25} iconName="ellipsis-vertical" />
+
+        </Pressable>
+
+        <ModalTopAnimation scale={animatedTop} isVisible={isModalVisible} onPress={resizeBox}></ModalTopAnimation>
     </View>
   )
 }
@@ -42,5 +67,9 @@ const style = StyleSheet.create({
     textContainer: {
         display: 'flex',
         flexDirection: 'column'
+    },
+    imageContainer: {
+        flexDirection: 'row',
+        gap: 0
     }
 })
