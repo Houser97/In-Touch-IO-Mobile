ecd21c521109b2f12e4c21d6997d273151f45d6d
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-01-12 09:06:06 -0600

    Add remaining screens

--- src/presentation/navigator/StackNavigator.tsx
+++ src/presentation/navigator/StackNavigator.tsx
@@ -1,16 +1,36 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import { HomeScreen } from '../screens/HomeScreen';
+import { HomeScreen } from '../screens/home/HomeScreen';
+import { LoginScreen } from '../screens/auth/LoginScreen';
+import { LoadingScreen } from '../screens/loading/LoadingScreen';
+import { ChatScreen } from '../screens/chat/ChatScreen';
+import { SearchScreen } from '../screens/search/SearchScreen';
+import { UserScreen } from '../screens/settings/UserScreen';
 
 export type RootStackParams = {
     HomeScreen: undefined;
+    LoginScreen: undefined;
+    LoadingScreen: undefined;
+    ChatScreen: undefined;
+    SearchScreen: undefined;
+    UserScreen: undefined;
 }
 
 const Stack = createStackNavigator<RootStackParams>();
 
 export const StackNavigator = () => {
     return(
-        <Stack.Navigator>
+        <Stack.Navigator 
+        initialRouteName='LoadingScreen'
+        screenOptions={{
+            headerShown: false,
+            headerStyle: {height: 60, }
+        }}>
             <Stack.Screen name='HomeScreen' component={HomeScreen} />
+            <Stack.Screen name='LoginScreen' component={LoginScreen} />
+            <Stack.Screen name='LoadingScreen' component={LoadingScreen} />
+            <Stack.Screen name='ChatScreen' component={ChatScreen} />
+            <Stack.Screen name='SearchScreen' component={SearchScreen} />
+            <Stack.Screen name='UserScreen' component={UserScreen} />
         </Stack.Navigator>
     )
 } 
