12af1258f877e626c166c25a8e85888f67996d13
Arturo Rivera <101058821+Houser97@users.noreply.github.com>
2025-01-26 16:53:38 -0600

    Create register method

--- src/presentation/store/auth/useAuthStore.ts
+++ src/presentation/store/auth/useAuthStore.ts
@@ -1,7 +1,7 @@
 import { create } from "zustand";
 import { User } from "../../../domain/entities/user.entity";
 import { AuthStatus } from "../../../infrastructure/interfaces/auth.status";
-import { authCheckStatus, authLogin } from "../../../actions/auth/auth";
+import { authCheckStatus, authLogin, authRegister } from "../../../actions/auth/auth";
 import { StorageAdapter } from "../../../config/helpers/storage.adapter";
 
 export interface AuthState {
@@ -11,6 +11,7 @@ export interface AuthState {
     errorMessage: string;
 
     login: (email: string, password: string) => Promise<boolean>;
+    register: (email: string, password: string, username: string) => Promise<boolean>;
     updateUser: (updatedUser: User) => void;
     checkStatus: () => Promise<boolean>;
     logout: () => Promise<void>;
@@ -24,6 +25,21 @@ export const useAuthStore = create<AuthState>()((set, get) => ({
 
     login: async(email: string, password: string) => {
         const {status, user, errorMessage, token} = await authLogin(email, password);
+        
+        if(!user){
+            set({status: 'unauthenticated', user, token, errorMessage });
+            return false;
+        }
+
+        await StorageAdapter.setItem('token', token);
+        
+        set({status: 'authenticated', user, token, errorMessage });
+
+        return true;
+    },
+
+    register: async(email: string, password: string, username: string) => {
+        const {status, user, errorMessage, token} = await authRegister(email, password, username);
         
         if(!user){
             set({status: 'unauthenticated', user, token, errorMessage });
